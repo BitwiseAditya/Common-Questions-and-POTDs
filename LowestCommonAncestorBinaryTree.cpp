8f087f347f491ce2f942d5bd80f3a0e5d8879f6a
Aditya Jugran <adityajugranreal@gmail.com>
2025-04-16 22:49:18 +0530

    lcatree

---
+++ LowestCommonAncestorBinaryTree.cpp
@@ -0,0 +1,77 @@
+/* 236. Lowest Common Ancestor of a Binary Tree
+
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the tree. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+    TreeNode *ans;
+
+public:
+    Solution()
+    {
+        ans = nullptr;
+    }
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        dfs(root, p, q);
+        return ans;
+    }
+    bool dfs(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == nullptr || root == NULL)
+            return false;
+        bool left = dfs(root->left, p, q);
+        bool right = dfs(root->right, p, q);
+        if (left == true && right == true)
+        {
+            ans = root;
+        }
+        else if ((left == true || right == true) && (root->val == p->val || root->val == q->val))
+        {
+            ans = root;
+        }
+        return left || right || root->val == p->val || root->val == q->val;
+    }
+};
