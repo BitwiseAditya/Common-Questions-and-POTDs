a2c64f40d63c796bd948e613e67f6beb7141d9af
Aditya Jugran <adityajugranreal@gmail.com>
2025-08-06 10:56:12 +0530

    RomanToInteger

---
+++ RomanToInteger.cpp
@@ -0,0 +1,53 @@
+/* Roman Number to Integer
+
+Given a string s in Roman number format, your task is to convert it to an integer. Various symbols and their values are given below.
+Note: I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+
+Examples:
+
+Input: s = "IX"
+Output: 9
+Explanation: IX is a Roman symbol which represents 10 – 1 = 9.
+Input: s = "XL"
+Output: 40
+Explanation: XL is a Roman symbol which represents 50 – 10 = 40.
+Input: s = "MCMIV"
+Output: 1904
+Explanation: M is 1000, CM is 1000 – 100 = 900, and IV is 4. So we have total as 1000 + 900 + 4 = 1904.
+Constraints:
+1 ≤ roman number ≤ 3999
+s[i] belongs to [I, V, X, L, C, D, M] */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int romanToDecimal(string &s)
+    {
+        // code here
+        unordered_map<char, int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i + 1 < n && mp[s[i]] < mp[s[i + 1]])
+            {
+                ans -= mp[s[i]];
+            }
+            else
+            {
+                ans += mp[s[i]];
+            }
+        }
+        return ans;
+    }
+};
