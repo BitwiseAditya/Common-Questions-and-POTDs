1a6efd4c140ee74bd10667df05ae251f2be4fccb
Aditya Jugran <adityajugranreal@gmail.com>
2025-11-04 12:09:21 +0530

    frogs jump up n up

---
+++ FrogJump.java
@@ -0,0 +1,49 @@
+/*Frog Jump
+
+Given an integer array height[] where height[i] represents the height of the i-th stair, a frog starts from the first stair and wants to reach the last stair. From any stair i, the frog has two options: it can either jump to the (i+1)th stair or the (i+2)th stair. The cost of a jump is the absolute difference in height between the two stairs. Determine the minimum total cost required for the frog to reach the last stair.
+
+Example:
+
+Input: heights[] = [20, 30, 40, 20]
+Output: 20
+Explanation:  Minimum cost is incurred when the frog jumps from stair 0 to 1 then 1 to 3:
+jump from stair 0 to 1: cost = |30 - 20| = 10
+jump from stair 1 to 3: cost = |20 - 30| = 10
+Total Cost = 10 + 10 = 20
+Input: heights[] = [30, 20, 50, 10, 40]
+Output: 30
+Explanation: Minimum cost will be incurred when frog jumps from stair 0 to 2 then 2 to 4:
+jump from stair 0 to 2: cost = |50 - 30| = 20
+jump from stair 2 to 4: cost = |40 - 50| = 10
+Total Cost = 20 + 10 = 30
+Constraints:
+1 ≤ height.size() ≤ 105
+0 ≤ height[i] ≤ 104 */
+
+package GFG_POTD;
+
+class FrogJump {
+    int minCost(int[] height) {
+        // code here
+        int n = height.length;
+
+        // DP array to store the minimum cost
+        int[] dp = new int[n];
+
+        // If there is only one stair,
+        // cost will be zero
+        dp[0] = 0;
+
+        // If there are 2 stairs, then frog
+        // can only take jump of size one
+        if (n > 1)
+            dp[1] = Math.abs(height[1] - height[0]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i - 1] + Math.abs(height[i] - height[i - 1]),
+                    dp[i - 2] + Math.abs(height[i] - height[i - 2]));
+        }
+
+        return dp[n - 1];
+    }
+}
